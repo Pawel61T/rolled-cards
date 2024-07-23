6e3a7d6a2323e39cc41f0a3177fbf93e9a226de1
Pawel61T <pawelddee@gmail.com>
2024-07-23 15:56:14 +0200

    xd

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 
-const firstCard = document.querySelector("#first-card")
+const firstCard = document.getElementById("first-card")
 const secondCard = document.getElementById("second-card")
 const thirdCard = document.getElementById("third-card")
-console.log(firstCard)
-console.log("hej")
-var cards = [firstCard, secondCard, thirdCard]
 
+var cards = [firstCard, secondCard, thirdCard]
+firstCard.addEventListener("click", () => {cardClicked(firstCard)})
+secondCard.addEventListener("click", () => {cardClicked(secondCard)})
+thirdCard.addEventListener("click", () => {cardClicked(thirdCard)})
+
+var lastSelected = firstCard
+
+function cardClicked(cardElement) {
+    cardElement.style.width = "80%";
+    console.log(`clicked ${cardElement.id}`)
+}

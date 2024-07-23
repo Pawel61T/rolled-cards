02549c9ff0308b19012a72ae49d879c7e9facad0
Pawel61T <pawelddee@gmail.com>
2024-07-23 14:58:51 +0200

    xd

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 
-const firstCard = document.getElementById("first-card")
+const firstCard = document.querySelector("#first-card")
 const secondCard = document.getElementById("second-card")
 const thirdCard = document.getElementById("third-card")
+console.log(firstCard)
+console.log("hej")
+var cards = [firstCard, secondCard, thirdCard]
 
-var selectedCard = firstCard
-
-function cardClicked(cardElement) {
-    
-}
